746afcb846ddd289cb048cc5108c2ddf485f51af
Kurt Van Dijck <kurt@vandijck-laurijssen.be>
2013-07-17 20:11:31 +0200

    const: add info applet

--- info.c
+++ info.c
@@ -25,8 +25,23 @@ static int libio_presets(int argc, char *argv[])
 	return 0;
 }
 
+static int libio_consts(int argc, char *argv[])
+{
+	const char *key = NULL;
+	int j;
+
+	if (argc > 1)
+		for (j = 1; j < argc; ++j)
+			printf("%s=%lf\n", argv[j], libio_const(argv[j]));
+	else
+		while ((key = libio_next_const(key)) != NULL)
+			printf("%s=%lf\n", key, libio_const(key));
+	return 0;
+}
+
 __attribute__((constructor))
 static void add_default_applets(void)
 {
 	register_applet("presets", libio_presets);
+	register_applet("consts", libio_consts);
 }
