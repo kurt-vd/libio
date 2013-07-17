3203dc8454e39e7e4f0e376196734fe40877c41a
Kurt Van Dijck <kurt@vandijck-laurijssen.be>
2013-07-17 20:03:16 +0200

    preset: add info applet

--- info.c
+++ info.c
@@ -11,7 +11,22 @@
 
 /* libio-related applets */
 
+static int libio_presets(int argc, char *argv[])
+{
+	const char *key = NULL;
+	int j;
+
+	if (argc > 1)
+		for (j = 1; j < argc; ++j)
+			printf("%s=%s\n", argv[j], libio_get_preset(argv[j]));
+	else
+		while ((key = libio_next_preset(key)) != NULL)
+			printf("%s=%s\n", key, libio_get_preset(key));
+	return 0;
+}
+
 __attribute__((constructor))
 static void add_default_applets(void)
 {
+	register_applet("presets", libio_presets);
 }
