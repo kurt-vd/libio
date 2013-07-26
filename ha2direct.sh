73547632a844f7b4a160a8b3836f265c8c79a214
Kurt Van Dijck <kurt@vandijck-laurijssen.be>
2013-07-26 05:59:18 +0200

    ha2: drop seperate lavabo

--- ha2direct.sh
+++ ha2direct.sh
@@ -4,7 +4,7 @@ exec io hadirect -l unix:@ha2 \
 	+led=oled \
 	+zolder=ozolder izolder \
 	+fan=ofan ibad1 \
-	+lavabo=olavabo ihead2 \
+	+lavabo=olavabo \
 	+bad=obad ibad4 \
 	+bluebad=oblubad \
 	+main=omain \
